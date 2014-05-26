f9919b3edf3ca9f84d5d5413e47c3fa8bb76066f
Olivier Godement <olivier.godement@gmail.com>
2014-05-26 19:04:08 +0200

    added new, create, edit and update methods and views to firms

--- app/controllers/firms_controller.rb
+++ app/controllers/firms_controller.rb
@@ -4,20 +4,38 @@ class FirmsController < ApplicationController
   end
 
   def show
+    @firm = Firm.find(params[:id])
   end
 
   def new
+    @firm = Firm.new
   end
 
   def create
+    @firm = Firm.new(firm_params)
+    @firm.save
+    redirect_to @firm
   end
 
   def edit
+    @firm = Firm.find(params[:id])
   end
 
   def update
+    @firm = Firm.find(params[:id])
+    @firm.update(firm_params)
+    redirect_to @firm
   end
 
   def destroy
   end
+
+  private
+
+    def firm_params
+      params.require(:firm).permit(:name, :official_address, :rcs, :court_service,
+             :bank_name, :bank_agency, :bank_agency_address, :bank_account, :valuation,
+             :share_price, :initial_capital, :shares)
+    end
+
 end
